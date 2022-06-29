9386de5e4429b51bc44db285d08696a028fa32e7
Edgar <edgaroboe@outlook.com>
2022-06-28 22:53:57 -0300

    remote origin rename

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { Alert, Keyboard, Modal, TouchableWithoutFeedback } from "react-native";
-import { Button } from "../../components/Form/Button";
-import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
-import { InputForm } from "../../components/Form/InputForm";
-import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
-import { Category } from "../../utils/category";
-import { CategorySelect } from "../CategorySelect";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Alert, Keyboard, Modal, TouchableWithoutFeedback } from 'react-native';
+import { Button } from '../../components/Form/Button';
+import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
+import { InputForm } from '../../components/Form/InputForm';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { Category } from '../../utils/category';
+import { CategorySelect } from '../CategorySelect';
 import {
   Container,
   Fields,
@@ -14,9 +14,10 @@ import {
   Header,
   Title,
   TransactionTypeButtonContainer,
-} from "./styles";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+} from './styles';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface FormData {
   name: string;
@@ -24,14 +25,15 @@ interface FormData {
 }
 
 const Scheme = Yup.object().shape({
-  name: Yup.string().required("Nome é obrigatório"),
+  name: Yup.string().required('Nome é obrigatório'),
   amount: Yup.number()
-    .typeError("Informe um valor numérico")
-    .positive("O valor não pode ser negativo")
-    .required("O valor é obrigatório"),
+    .typeError('Informe um valor numérico')
+    .positive('O valor não pode ser negativo')
+    .required('O valor é obrigatório'),
 });
 
 export function Register() {
+  const collectionKey = '@gofinances:transactions';
   const {
     control,
     handleSubmit,
@@ -40,18 +42,18 @@ export function Register() {
     resolver: yupResolver(Scheme),
   });
   const [transactionType, setTransactionType] = useState<
-    "income" | "outcome" | null
+    'income' | 'outcome' | null
   >(null);
   const [category, setCategory] = useState<Category>();
   const [isCategorySelectOpen, setCategorySelectOpen] = useState(false);
 
-  function handleTransactionTypeSelect(type: "income" | "outcome") {
+  function handleTransactionTypeSelect(type: 'income' | 'outcome') {
     setTransactionType(type);
   }
 
-  function handleRegister(form: FormData) {
-    if (!transactionType) return Alert.alert("Selecione o tipo de transação");
-    if (!category?.key) return Alert.alert("Selecione a categoria");
+  async function handleRegister(form: FormData) {
+    if (!transactionType) return Alert.alert('Selecione o tipo de transação');
+    if (!category?.key) return Alert.alert('Selecione a categoria');
 
     const data = {
       name: form.name,
@@ -59,12 +61,18 @@ export function Register() {
       transactionType,
       category: category?.key,
     };
-    console.log(data);
+
+    try {
+      await AsyncStorage.setItem(collectionKey, JSON.stringify(data));
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível salvar');
+    }
   }
 
   return (
     <TouchableWithoutFeedback
-      style={{ flex: 1, width: "100%" }}
+      style={{ flex: 1, width: '100%' }}
       onPress={Keyboard.dismiss}
     >
       <Container>
@@ -90,22 +98,22 @@ export function Register() {
             />
             <TransactionTypeButtonContainer>
               <TransactionTypeButton
-                selected={transactionType === "income"}
+                selected={transactionType === 'income'}
                 title="Income"
                 type="income"
-                onPress={() => handleTransactionTypeSelect("income")}
+                onPress={() => handleTransactionTypeSelect('income')}
                 activeOpacity={1}
               />
               <TransactionTypeButton
-                selected={transactionType === "outcome"}
+                selected={transactionType === 'outcome'}
                 title="Outcome"
                 type="outcome"
-                onPress={() => handleTransactionTypeSelect("outcome")}
+                onPress={() => handleTransactionTypeSelect('outcome')}
                 activeOpacity={1}
               />
             </TransactionTypeButtonContainer>
             <CategorySelectButton
-              title={category?.name || "Categoria..."}
+              title={category?.name || 'Categoria...'}
               onPress={() => setCategorySelectOpen(true)}
               hasValue={!!category}
             />
